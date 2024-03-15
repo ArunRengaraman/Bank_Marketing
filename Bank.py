f133587f8570bef101c2a607200299024ea5be73
ArunRengaraman <62067339+ArunRengaraman@users.noreply.github.com>
2024-03-15 17:12:38 -0400

    Update Bank.py

--- Bank.py
+++ Bank.py
@@ -25,7 +25,7 @@ dataset_name = st.sidebar.selectbox(
 st.write(f"## {dataset_name} Dataset")
 
 classifier_name = st.sidebar.selectbox(
-    'Select classifier', ('GB', 'SVM', 'Random Forest')
+    'Select classifier', ('GB', 'SVM', 'Random Forest', 'MLP')
 )
 
 def get_dataset(name):
