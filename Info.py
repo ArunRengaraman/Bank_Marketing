8fd3dca2e2ccc6d601dfb309f4f6ab0faf085973
ArunRengaraman <62067339+ArunRengaraman@users.noreply.github.com>
2024-03-17 10:34:36 -0400

    Update Info.py

--- Info.py
+++ Info.py
@@ -23,10 +23,8 @@ The data is related to direct marketing campaigns of a Portuguese banking instit
 
 There are four datasets available:
 
-1. **bank-additional-full.csv**: Contains all examples (41,188) and 20 inputs, ordered by date (from May 2008 to November 2010), very close to the data analyzed in [Moro et al., 2014].
-2. **bank-additional.csv**: Contains 10% of the examples (4,119), randomly selected from the first dataset, with 20 inputs.
-3. **bank-full.csv**: Contains all examples and 17 inputs, ordered by date (an older version of the dataset with fewer inputs).
-4. **bank.csv**: Contains 10% of the examples and 17 inputs, randomly selected from the third dataset (an older version of the dataset with fewer inputs).
+ **bank-full.csv**: Contains all examples and 17 inputs, ordered by date (an older version of the dataset with fewer inputs).
+
 
 The smaller datasets are provided to test more computationally demanding machine learning algorithms (e.g., SVM).
 
