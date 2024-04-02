25fefb94d9a6cd0fb7eb387a682bc4d1cee95b5f
ArunRengaraman <62067339+ArunRengaraman@users.noreply.github.com>
2024-04-02 17:31:36 -0400

    Update Info.py

--- Info.py
+++ Info.py
@@ -13,8 +13,6 @@ def app():
 
         The data is related to direct marketing campaigns of a Portuguese banking institution. The marketing campaigns were based on phone calls. Often, more than one contact to the same client was required to assess if the product (bank term deposit) would be subscribed ('yes') or not ('no').
 
-        There are four datasets available:
-
          **bank-full.csv**: Contains all examples and 17 inputs, ordered by date (an older version of the dataset with fewer inputs).
 
         The classification goal is to predict if the client will subscribe (yes/no) a term deposit (variable y).
