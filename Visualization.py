0e1bc6f4730dfa44c9316abb04786beb25cf3295
ArunRengaraman <62067339+ArunRengaraman@users.noreply.github.com>
2024-04-03 22:56:47 -0400

    Update Visualization.py

--- Visualization.py
+++ Visualization.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import warnings
+warnings.filterwarnings("ignore")
 
 def app():
     st.title('Data Visualization')
